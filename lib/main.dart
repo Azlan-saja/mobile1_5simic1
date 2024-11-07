453b5203f38ac93909c96a4bf87c3358ed128099
Azlan-saja <azlansaja19@gmail.com>
2024-11-07 21:12:09 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController nilaiPanjang = TextEditingController();
+    TextEditingController nilaiLebar = TextEditingController();
+    TextEditingController hasilLuas = TextEditingController();
+
     return MaterialApp(
       theme: ThemeData(
         useMaterial3: true,
@@ -17,18 +21,77 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Aplikasi 5SIMIC1'),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text(
+            'Aplikasi 5SIMIC1',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Colors.blue[900],
         ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text('1'),
-            Text('2'),
-            Text('3'),
-            Text('4'),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nilaiPanjang,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Nilai Panjang',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+              TextFormField(
+                controller: nilaiLebar,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Nilai Lebar',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  double xhasil = 0;
+                  xhasil = double.parse(nilaiPanjang.text) *
+                      double.parse(nilaiLebar.text);
+                  hasilLuas.text = xhasil.toString();
+                },
+                child: const Text(
+                  'Luas',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[900],
+                ),
+              ),
+              TextFormField(
+                controller: hasilLuas,
+                decoration: const InputDecoration(
+                  labelText: 'Hasil Luas',
+                  icon: Icon(Icons.numbers),
+                  hintText: '0',
+                ),
+              ),
+            ],
+          ),
         ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Akun',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.exit_to_app),
+            label: 'Keluar',
+          ),
+        ]),
       ),
     );
   }
